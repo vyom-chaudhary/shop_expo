4c2c8951d2cb240b87c33144f3cd20340b33b8dc
vyom-chaudhary <vyom@itservicesindia.com>
2021-02-08 18:03:03 +0530

    data

--- App.js
+++ App.js
@@ -1,14 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { enableScreens } from 'react-native-screens'
+
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import AppLoading from 'expo-app-loading'
+import * as Font from 'expo-font'
+import productReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
+import orderReducer from './store/reducers/orders'
+
+import ShopNavigator from './navigation/ShopNavigator';
+import DrawerNavigator from './navigation/DrawerNavigator'
+import { composeWithDevTools } from 'redux-devtools-extension'
+enableScreens();
+
+
+
+const rootReducer = combineReducers({
+  products: productReducer,
+  cart: cartReducer,
+  orders: orderReducer
+})
+
+const store = createStore(rootReducer, composeWithDevTools())
+const FetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontLoading, setFontLoading] = useState(false)
+
+  if (!fontLoading) {
+    return (<AppLoading startAsync={FetchFonts} onFinish={() => setFontLoading(true)} onError={() => console.log('font loading in error')}></AppLoading>)
+  } else {
+    return (
+      <Provider store={store}>
+        <NavigationContainer>
+          <DrawerNavigator>
+          </DrawerNavigator>
+        </NavigationContainer>
+
+      </Provider>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
